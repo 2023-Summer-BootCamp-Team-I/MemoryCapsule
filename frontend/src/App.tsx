60f6abe9115bf5001f5e0200d81bc0360e5fd285
devGEP <ju0032564@gmail.com>
2023-07-05 23:32:00 +0900

    chore: fix prettier error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,7 +20,7 @@ function App() {
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p className="text-sm">
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR Wow
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
