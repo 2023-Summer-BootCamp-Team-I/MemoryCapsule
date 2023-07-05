dd32d9d084a91d49e05d5daa75d892d495bad913
devGEP <ju0032564@gmail.com>
2023-07-05 17:11:58 +0900

    feat: 필요한 라이브러리 설치

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // eslint-disable-next-line quotes
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
