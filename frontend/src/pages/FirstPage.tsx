e28fc2fb7215758ee5badd07db52bfacb5580552
1-yuna <97020423+1-yuna@users.noreply.github.com>
2023-07-08 04:59:53 +0900

    feat: add login/join text box

--- frontend/src/pages/FirstPage.tsx
+++ frontend/src/pages/FirstPage.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import OpenButton from '../components/OpenButton';
+
+// 로그인/ 회원가입
+// import TextInput from '../components/TextInput';
+
+// <form method="post" action="서버의url" id="login-form">
+//         <TextInput label="ID" placeholder="아이디를 입력해주세요" title="login" />
+//         <TextInput label="비밀번호" placeholder="아이디를 입력해주세요" title="join" />
+// </form>
 
 export default function FirstPage() {
   return (
     <div>
       <div>FirstPage</div>
-      <OpenButton imageName="video" />
     </div>
   );
 }
