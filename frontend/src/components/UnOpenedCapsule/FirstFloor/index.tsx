42e5d4ddadb53580f1eef3ad605275e3d5627903
tjdgns2113 <tjdgns3546@naver.com>
2023-07-10 21:52:41 +0900

    chore: start UnOpenedCapsule Page

---
+++ frontend/src/components/UnOpenedCapsule/FirstFloor/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function FirstFloor() {
+  return <div>하하</div>;
+}
+
+export default FirstFloor;
--- frontend/src/pages/UnOpenedCapsulePage.tsx
+++ frontend/src/pages/UnOpenedCapsulePage.tsx
@@ -1,11 +1,13 @@
-import StoryModal from '../components/common/StoryModal';
+// import StoryModal from '../components/common/StoryModal';
+import FirstFloor from '../components/UnOpenedCapsule/FirstFloor';
 
 export default function UnOpenedCapsulePage() {
   return (
     <div>
       {/* UnOpenedCapsulePage */}
-      <StoryModal title="Detail" content="detail" />
-      <StoryModal title="Create" content="create" />
+      {/* <StoryModal title="Detail" content="detail" />
+      <StoryModal title="Create" content="create" /> */}
+      <FirstFloor />
     </div>
   );
 }
