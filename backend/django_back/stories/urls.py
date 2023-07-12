2a38084f3f73a0b986558d16af57ae5d7c334859
Kiminni <imk0980@gmail.com>
2023-07-13 00:45:18 +0900

    fix: 함수 이름 변경, 스토리 리스트 api 수정

--- backend/django_back/stories/urls.py
+++ backend/django_back/stories/urls.py
@@ -1,9 +1,9 @@
 from rest_framework.decorators import api_view
 from django.urls import path
-from .views import story_func, story_POST #story_list_GET
+from .views import story_func, story_capsule_func #story_list_GET
 
 urlpatterns = [
-    path('<int:capsule_id>', story_POST),
+    path('<int:capsule_id>', story_capsule_func),
     path('<int:capsule_id>/<int:story_id>', story_func),
     #path('<int:capsule_id>/<int:story_id>', story_update),
     #path('<int:capsule_id>/<int:story_id>', story_delete)
