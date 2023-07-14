b350d5a37820df223b1b8be1d8f34dac6e3f4490
devGEP <ju0032564@gmail.com>
2023-07-14 14:25:46 +0900

    chore: 504 에러 해결

--- frontend/src/pages/FirstPage.tsx
+++ frontend/src/pages/FirstPage.tsx
@@ -12,8 +12,17 @@ import Login from '../components/Login';
 import JoinModal from '../components/JoinModal';
 import TreeLottie from '../components/TreeLottie';
 
+import { useNavigate } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { loggedInState } from '../utils/Recoil';
+import PasswordModal from '../components/MainUnopenCapsule/PasswordModal'; // 비밀
+
 const FirstPage: React.FC = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const [loggedIn, setLoggedIn] = useRecoilState(loggedInState);
+  const navigate = useNavigate();
+  const [capsuleId, setCapsuleId] = useState<string>('');
 
   const onSignUp = () => {
     setIsModalOpen(true);
@@ -23,23 +32,15 @@ const FirstPage: React.FC = () => {
     setIsModalOpen(false);
   };
 
-  import { useNavigate } from 'react-router-dom';
-  import { useRecoilState } from 'recoil';
-  import { loggedInState } from '../utils/Recoil';
-  import PasswordModal from '../components/MainUnopenCapsule/PasswordModal'; // 비밀번호 입력 모달 컴포넌트 경로를 입력해주세요
-  const [loggedIn, setLoggedIn] = useRecoilState(loggedInState);
-  const navigate = useNavigate();
-  const [isModalOpen, setModalOpen] = useState<boolean>(false);
-  const [capsuleId, setCapsuleId] = useState<string>('');
   const handleClick = () => {
     const storedCapsuleId = sessionStorage.getItem('capsule_id');
     if (storedCapsuleId) {
       setCapsuleId(storedCapsuleId);
-      setModalOpen(true);
+      setIsModalOpen(true);
+      // eslint-disable-next-line no-console
       console.log('isLoggedIn: ', loggedIn);
     } else {
       setLoggedIn(true);
-      // eslint-disable-next-line no-console
       // eslint-disable-next-line no-console
       console.log('isLoggedIn: ', loggedIn);
       navigate('/mainunopened');
@@ -47,44 +48,44 @@ const FirstPage: React.FC = () => {
   };
   return (
     <div className="h-[42rem] w-[75rem]">
-      <div className="h-1/4 flex">
-        <div className="w-1/3 flex justify-end">
+      <div className="flex h-1/4">
+        <div className="flex justify-end w-1/3">
           <div className="relative mr-10">
             <img className="w-20 h-20 " src={flowerImg1} />
           </div>
         </div>
-        <div className="w-1/3 flex items-center justify-center">
+        <div className="flex items-center justify-center w-1/3">
           <div className="relative -mb-10">
-            <img className="w-28 h-20" src={cloudImg} />
+            <img className="h-20 w-28" src={cloudImg} />
           </div>
         </div>
-        <div className="w-1/3 flex justify-start relative -mt-5">
+        <div className="relative flex justify-start w-1/3 -mt-5">
           <div className="relative -mt-5">
             <img className="w-24 h-24" src={flowerImg2} />
           </div>
         </div>
       </div>
-      <div className="flex h-3/4 pr-16">
-        <div className="flex flex-col items-center justify-around p-6 w-1/3">
-          <img className="w-16 h-16 relative -ml-32" src={flowerImg4} />
+      <div className="flex pr-16 h-3/4">
+        <div className="flex flex-col items-center justify-around w-1/3 p-6">
+          <img className="relative w-16 h-16 -ml-32" src={flowerImg4} />
           <TreeLottie />
         </div>
-        <div className="w-1/2 flex flex-col justify-around  pb-10">
+        <div className="flex flex-col justify-around w-1/2 pb-10">
           <div className="font-Omu">
-            <h1 className=" text-9xl mr-24 font-bold italic">memory</h1>
-            <h1 className="text-9xl ml-24 font-bold italic">capsule</h1>
+            <h1 className="mr-24 italic font-bold text-9xl">memory</h1>
+            <h1 className="ml-24 italic font-bold text-9xl">capsule</h1>
           </div>
           <Login onSignUp={onSignUp} />
           {isModalOpen && <JoinModal onClose={closeModal} />}
         </div>
         <div className="flex flex-col items-center justify-around w-1/6 ">
-          <img className="w-16 h-16 relative -mr-32" src={flowerImg3} />
-          <img className="w-16 h-16 relative -ml-20" src={flowerImg4} />
+          <img className="relative w-16 h-16 -mr-32" src={flowerImg3} />
+          <img className="relative w-16 h-16 -ml-20" src={flowerImg4} />
         </div>
       </div>
       <button onClick={handleClick}>로그인</button>
       {isModalOpen && (
-        <PasswordModal capsuleId={capsuleId} closeModal={() => setModalOpen(false)} />
+        <PasswordModal capsuleId={capsuleId} closeModal={() => setIsModalOpen(false)} />
       )}
     </div>
   );
