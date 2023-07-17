1edc6d191df7b6c22ed41eddecbbb650cc701168
carrotcountry <jaeyoon321@naver.com>
2023-07-17 17:21:43 +0900

    refactor: user 회원가입 쿼리문으로 중복 관리, 이메일 형식 관리

--- backend/django_back/users/models.py
+++ backend/django_back/users/models.py
@@ -16,7 +16,7 @@ class User(BaseModel):
     email = models.CharField(unique=True, max_length=200, null=True, blank=True)
     nickname = models.CharField(max_length=60)
     status = models.IntegerField(default=0)
-    user_img_url = models.CharField(max_length=255)
+    user_img_url = models.CharField(max_length=255, null=True, blank=True)
 
 
     class Meta:
