fdb43b1aaf1ee814785d142a73fb879c2995a601
Kiminni <imk0980@gmail.com>
2023-07-24 18:29:28 +0900

    fix: jwt 토큰 되게 수정

--- backend/django_back/stories/views.py
+++ backend/django_back/stories/views.py
@@ -7,8 +7,6 @@ from images.views import upload_image_for_api
 from django.http import HttpResponse, JsonResponse
 from django.utils import timezone
 from core.uuid_decode import *
-
-
 # Create your views here.
 
 
@@ -55,11 +53,14 @@ def story_capsule_func(request, capsule_id):
             "created_at": story.created_at,
             "updated_at": story.updated_at,
             "story_id": story.story_id,
-            "creator_id": story.creator_id
+            "creator_id": story.creator_id,
+            "is_mine" : story.creator_id == user.user_id
         })
         return JsonResponse({"code": 400, "message": "스토리 생성에 실패하였습니다."}, status=400)
 
     elif request.method =='GET':
+        user_uuid_obj = get_user_uuid_obj_from_jwt(request.GET.get('jwt_token', None))
+
         try:
             capsule = Capsule.objects.get(capsule_id=capsule_id)
         except Capsule.DoesNotExist:
@@ -70,7 +71,9 @@ def story_capsule_func(request, capsule_id):
             story_info = {
                 "story_id" : story.story_id,
                 "story_title" : story.story_title,
-                "story_url" : story.story_img_url
+                "story_url" : story.story_img_url,
+                "creator" : story.creator_id,
+                "is_mine": story.creator_id == user_uuid_obj
             }
 
             story_list.append(story_info)
@@ -80,6 +83,7 @@ def story_capsule_func(request, capsule_id):
             "message": "스토리 이미지 URL 리스트 조회가 완료되었습니다.",
             "capsule_id": capsule.capsule_id,
             "story_list": story_list
+
             })
 
 
@@ -88,55 +92,87 @@ def story_capsule_func(request, capsule_id):
 def story_func(request, capsule_id, story_id) :
 
     if request.method == 'GET' :
+        user_uuid_obj = get_user_uuid_obj_from_jwt(request.GET.get('jwt_token', None))
+
         try:
             story = Story.objects.get(story_id = story_id, deleted_at__isnull=True)
             capsule = Capsule.objects.get(capsule_id = capsule_id)
         except Story.DoesNotExist:
             return JsonResponse({"code": 404, "message": "스토리가 존재하지 않습니다."}, status=404)
-
+        if story.creator_id != user_uuid_obj:
+            return JsonResponse({"code" : 403,
+                                 "message" : "올바른 ID가 아닙니다."})
         return JsonResponse({
             "code": 200,
             "message": "스토리 조회가 완료되었습니다.",
+            "creator_id": story.creator_id,
             "capsule_id" : capsule.capsule_id,
             "story_title": story.story_title,
             "story_content": story.story_content,
             "story_img_url": story.story_img_url,
             "created_at": story.created_at,
             "updated_at": story.updated_at,
-            "story_id": story.story_id
+            "story_id": story.story_id,
+            "is_mine" : story.creator_id == user_uuid_obj
         })
 
-    elif request.method == 'PUT' :
+    elif request.method == 'PUT':
+        user_uuid_obj = get_user_uuid_obj_from_jwt(request.POST['jwt_token'])
+        user = User.objects.get(pk=user_uuid_obj)
         try:
             story = Story.objects.get(story_id = story_id)
         except Story.DoesNotExist:
             return JsonResponse({"code": 404, "message": "스토리가 존재하지 않습니다."}, status=404)
 
+        authenticated_user_id = user.user_id
+        if story.creator_id == authenticated_user_id:
+            story.story_title = request.data.get('story_title')
+            story.story_content = request.data.get('story_content')
+            story.story_img_url = upload_image_for_api(request.FILES['filename'])
+            story.updated_at = request.data.get('updated_at')
+            story.save()
 
-        story.story_title = request.data.get('story_title')
-        story.story_content = request.data.get('story_content')
-        story.story_img_url = upload_image_for_api(request.FILES['filename'])
-        story.updated_at = request.data.get('updated_at')
-        story.save()
-
-        return JsonResponse({
-            "code": 200,
-            "message": "스토리 수정이 완료되었습니다.",
-            "creator": story.creator_id,
-            "story_id": story.story_id,
-            "story_title": story.story_title,
-            "story_content": story.story_content,
-            "story_img_url": story.story_img_url,
-            "updated_at": story.updated_at,
-        })
+            return JsonResponse({
+                "code": 200,
+                "message": "스토리 수정이 완료되었습니다.",
+                "creator": story.creator_id,
+                "story_id": story.story_id,
+                "story_title": story.story_title,
+                "story_content": story.story_content,
+                "story_img_url": story.story_img_url,
+                "updated_at": story.updated_at,
+            })
+        else:
+            return JsonResponse({
+                'code': 403,
+                'message': '스토리 삭제 권한이 없습니다.'
+            }, status=403)
 
         return JsonResponse({"code": 400, "message": "스토리 수정에 실패하였습니다."}, status=400)
 
     elif request.method == 'DELETE':
+        user_uuid_obj = get_user_uuid_obj_from_jwt(request.GET.get('jwt_token', None))
+
         try:
             story = Story.objects.get(story_id=story_id, deleted_at__isnull=True)
+
+            if story.creator_id == user_uuid_obj:
+                story.deleted_at = timezone.now()
+                story.save()
+                return JsonResponse({
+                    'code': 200,
+                    'message': '스토리 삭제 완료',
+                    'deleted_at': story.deleted_at,
+                    'time': timezone.now().strftime('%Y-%m-%d %H:%M:%S')
+                })
+            else:
+                return JsonResponse({
+                    'code': 403,
+                    'message': '스토리 삭제 권한이 없습니다.'
+                }, status=403)
             story.deleted_at = timezone.now()
             story.save()
+
             return JsonResponse({
                 'code': 200, 'message': '스토리 삭제 완료',
                 'deleted_at': story.deleted_at,
