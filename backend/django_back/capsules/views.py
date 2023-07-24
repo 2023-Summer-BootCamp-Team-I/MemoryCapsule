0dde56e852ee4dd076ad90098241b292baedeff7
JaeBin <201924576@pusan.ac.kr>
2023-07-24 20:50:06 +0900

    feat: swagger capsule form data 추가

--- backend/django_back/capsules/views.py
+++ backend/django_back/capsules/views.py
@@ -1,10 +1,90 @@
 import json
 from django.http import JsonResponse
 from .utils import *
+from rest_framework.decorators import api_view, parser_classes
+from rest_framework.parsers import MultiPartParser, FormParser
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 
+@swagger_auto_schema(
+    methods=['GET'],
+    tags=["Capsule 전체 조회"],
+    manual_parameters=[
+        openapi.Parameter(
+            name="jwt_token",
+            in_=openapi.IN_QUERY,
+            type=openapi.TYPE_STRING,
+            description="jwt token 입력",
+        ),
+        openapi.Parameter(
+            name="is_open",
+            in_=openapi.IN_QUERY,
+            type=openapi.TYPE_STRING,
+            description="캡슐이 열렸는지 체크",
+        ),
+        openapi.Parameter(
+            name="count",
+            in_=openapi.IN_QUERY,
+            type=openapi.TYPE_STRING,
+            description="가져올 캡슐 개수. -1을 입력 시, 모든 캡슐 반환 ",
+        ),
+    ]
+)
+@swagger_auto_schema(
+    methods=['POST'],
+    tags=["Capsule 생성"],
+    consumes=['multipart/form-data'],
+    manual_parameters=[
+        openapi.Parameter(
+            name="jwt_token",
+            in_=openapi.IN_FORM,
+            type=openapi.TYPE_STRING,
+            description="jwt token 입력",
+        ),
+        openapi.Parameter(
+            name="theme_id",
+            in_=openapi.IN_FORM,
+            type=openapi.TYPE_INTEGER,
+            description="테마 아이디",
+        ),
+        openapi.Parameter(
+            name="capsule_name",
+            in_=openapi.IN_FORM,
+            type=openapi.TYPE_STRING,
+            description="캡슐 이름",
+        ),
+        openapi.Parameter(
+            name="due_date",
+            in_=openapi.IN_FORM,
+            type=openapi.TYPE_STRING,
+            format=openapi.FORMAT_DATETIME,
+            description="캡슐 개봉일 Due Date",
+        ),
+        openapi.Parameter(
+            name="limit_count",
+            in_=openapi.IN_FORM,
+            type=openapi.TYPE_INTEGER,
+            description="영상에 사용할 이미지 개수 제한",
+        ),
+        openapi.Parameter(
+            name="capsule_password",
+            in_=openapi.IN_FORM,
+            type=openapi.TYPE_STRING,
+            description="캡슐 비밀번호",
+        ),
+        openapi.Parameter(
+            name="img_file",
+            in_=openapi.IN_FORM,
+            type=openapi.TYPE_FILE,
+            description="캡슐 이미지 파일",
+        ),
+
+    ]
+)
 # /api/v1/capsules/capsule_func/
 @api_view(['GET', 'POST'])
+@parser_classes([MultiPartParser, FormParser])
 def capsule_func(request) -> json:
     result: json
     status_code: int
