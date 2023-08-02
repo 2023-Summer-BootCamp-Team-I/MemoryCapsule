94a65ec6311f78fe48fe50492d5d82057ec19dcf
JaeBin <201924576@pusan.ac.kr>
2023-08-03 00:15:36 +0900

    test: test

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -15,6 +15,6 @@ cd $REPOSITORY
 
 docker-compose -f $COMPOSE_FILE down -v --rmi "all"
 docker-compose -f $ELK_COMPOSE_FILE down -v --rmi "all"
-find . -path "*/migrations/*.py" -delete
+sudo find . -path "*/migrations/*.py" -delete
 
 
